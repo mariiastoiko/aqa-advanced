87beba888897f95afb2f40b7274499ef47b777a9
Mariia Stoiko <havrysm@gmail.com>
2024-09-07 23:56:07 +0300

    home work 4.1. JavaScript primitive data types

---
+++ lesson-4-1/jsBasics/task1.js
@@ -0,0 +1,15 @@
+const stringValue = "home work";
+const numberValue = 123;
+const booleanValue = false;
+const undefinedValue = undefined;
+const nullValue = null;
+const symbolValue = Symbol("abc") // description
+const bigIntValue = 11235678696543n;
+
+console.log(stringValue)
+console.log(numberValue)
+console.log(booleanValue)
+console.log(undefinedValue)
+console.log(nullValue)
+console.log(symbolValue.toString())
+console.log(bigIntValue)
---
+++ lesson-4-1/jsBasics/task4.js
@@ -0,0 +1,14 @@
+const radius = 10
+console.log(Math.PI * radius * radius)
+
+
+const length = 10
+const width = 5
+console.log(length * width)
+
+
+const height = 8
+const result = Math.PI * radius*radius * height
+console.log(result)
+console.log(Number.parseFloat(result).toFixed(2))
+
