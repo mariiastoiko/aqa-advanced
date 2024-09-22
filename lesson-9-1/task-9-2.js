387d5e1ef74ba1f3d09bb4bd445a538da45aa0e4
Mariia Stoiko <havrysm@gmail.com>
2024-09-22 23:03:37 +0300

    homework  9 Objects in JavaScript

---
+++ lesson-9-1/task-9-2.js
@@ -0,0 +1,8 @@
+const book = {
+    title: 'The Secret History', 
+    author: 'Donna Tartt',
+    year: 1992
+}
+const {title, author} = book
+console.log(title)
+console.log(author)
---
+++ lesson-9-1/task-9-4.js
@@ -0,0 +1,9 @@
+const person = {
+    firstName: 'Harry',
+    lastName: 'Potter',
+    age: 18
+}
+person.email = 'hp@gmail.com'
+delete person.age;
+
+console.log(person)
